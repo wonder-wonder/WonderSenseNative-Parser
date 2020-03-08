9b1e0d6415a90d359f01b16fc8df0575106d34bf
marsh31 <marsh22h53@gmail.com>
2020-03-08 12:04:21 +0900

    add structure

--- parser.cpp
+++ parser.cpp
@@ -1,12 +1,24 @@
 #include "parser.hpp"
+#include <cstdint>
 
 struct data_packet_t {
-  short ax, ay, az;      // 6 bytes
-  short gx, gy, gz;      // 6 bytes
-  short mx, my, mz;      // 6 bytes
-  short temperature;     // 2 bytes
-  short battery;         // 2 bytes
-  short pressSensorVol;  // 2 bytes //total size:24 bytes
+  short ax, ay, az;   // 6 bytes
+  short gx, gy, gz;   // 6 bytes
+  short mx, my, mz;   // 6 bytes
+  short temperature;  // 2 bytes
+  short battery;      // 2 bytes
+  short airPressure;  // 2 bytes //total size:24 bytes
 };
 
+
+struct sensor_property {
+  std::uint16_t dataRate;
+  std::uint8_t accFsr;
+  std::uint16_t gyroFsr;
+  std::uint16_t dataRate;
+  float magXcoef, magYcoef, magZcoef;
+  std::uint8_t lpf;
+};
+
+void init_parser(void *data) {}
 void parse(void *data) {}
--- parser.hpp
+++ parser.hpp
@@ -1,15 +1,38 @@
 #ifndef PARSER_HEADER
 #define PARSER_HEADER
 
+#include <cstdint>
+#include <string>
+
 struct sensor_data {
   double ax, ay, az;
   double gx, gy, gz;
   double mx, my, mz;
-  double temp;
+  double temperature;
   double battery;
-  double press;
+  double airPressure;
 };
 
+
+struct sensor_property {
+  std::uint16_t dataRate;
+  std::uint8_t accFsr;
+  std::uint16_t gyroFsr;
+  std::uint16_t dataRate;
+  float magXcoef, magYcoef, magZcoef;
+  std::uint8_t lpf;
+};
+
+
+struct sensor_devInfo {
+  std::string manufacturer;
+  std::string hardware;
+  std::string firmware;
+  std::string address;
+};
+
+
+extern void init_parser(void *data);
 extern void parse(void *data);
 
 #endif /* end of include guard: PARSER_HEADER */
