a2ae1cd9fa519df6df2e0d2ea25b30ce94b6ece8
marsh31 <marsh22h53@gmail.com>
2020-07-25 19:51:22 +0900

    fix parser

--- parser.cpp
+++ parser.cpp
@@ -1,53 +1,49 @@
 #include "parser.hpp"
-#include <cstdint>
 
-// define
-struct data_packet_t {
-  short ax, ay, az;   // 6 bytes
-  short gx, gy, gz;   // 6 bytes
-  short mx, my, mz;   // 6 bytes
-  short temperature;  // 2 bytes
-  short battery;      // 2 bytes
-  short airPressure;  // 2 bytes //total size:24 bytes
-};
+constexpr int INT12_MAX = 4096;
 
 
-// values
-sensor_property m_property;
-
-
-// functions
-void init_parser(void *data)
+Parser::Parser(const SensorProperty &property)
 {
-  m_property = *(sensor_property *)data;
+  mProperty = property;
 }
 
-
-constexpr int packet_size = 4;
-sensor_data *parse(void *data)
+const std::array<SensorData, c_packet_size> Parser::parse(
+    std::uint8_t data[c_data_size])
 {
   data_packet_t *data_packets;
-  data_packets = (data_packet_t *)malloc(sizeof(data_packet_t) * 4);
-  data_packets = (data_packet_t *)data;
+  data_packets = reinterpret_cast<data_packet_t *>(data);
 
-  sensor_data *ret;
-  ret = (sensor_data *)malloc(sizeof(sensor_data) * 4);
+  std::array<SensorData, c_packet_size> ret;
+  for (int i = 0; i < c_packet_size; ++i) {
+    ret[i].ax = (static_cast<double>(data_packets[i].ax * mProperty.accFsr)
+                 / static_cast<double>(INT16_MAX + 1));
 
-  for (int i = 0; i < packet_size; ++i) {
-    ret[i].ax = (data_packets[i].ax * m_property.accFsr / 32768.0);
-    ret[i].ay = (data_packets[i].ay * m_property.accFsr / 32768.0);
-    ret[i].az = (data_packets[i].az * m_property.accFsr / 32768.0);
+    ret[i].ay = (static_cast<double>(data_packets[i].ay * mProperty.accFsr)
+                 / static_cast<double>(INT16_MAX + 1));
 
-    ret[i].gx = (data_packets[i].gx * m_property.gyroFsr / 32768.0);
-    ret[i].gy = (data_packets[i].gy * m_property.gyroFsr / 32768.0);
-    ret[i].gz = (data_packets[i].gz * m_property.gyroFsr / 32768.0);
+    ret[i].az = (static_cast<double>(data_packets[i].az * mProperty.accFsr)
+                 / static_cast<double>(INT16_MAX + 1));
 
-    ret[i].mx = (data_packets[i].mx * 0.15 * m_property.magXcoef);
-    ret[i].my = (data_packets[i].my * 0.15 * m_property.magYcoef);
-    ret[i].mz = (data_packets[i].mz * 0.15 * m_property.magZcoef);
+    ret[i].gx = (static_cast<double>(data_packets[i].gx * mProperty.gyroFsr)
+                 / static_cast<double>(INT16_MAX + 1));
 
-    ret[i].battery     = data_packets[i].battery / 4096.0 * 3.3 * 4.9;
-    ret[i].airPressure = data_packets[i].airPressure / 4096.0 * 3.3;
+    ret[i].gy = (static_cast<double>(data_packets[i].gy * mProperty.gyroFsr)
+                 / static_cast<double>(INT16_MAX + 1));
+
+    ret[i].gz = (static_cast<double>(data_packets[i].gz * mProperty.gyroFsr)
+                 / static_cast<double>(INT16_MAX + 1));
+
+    ret[i].mx = ((data_packets[i].mx * 0.15) * mProperty.magXcoef);
+    ret[i].my = ((data_packets[i].my * 0.15) * mProperty.magYcoef);
+    ret[i].mz = ((data_packets[i].mz * 0.15) * mProperty.magZcoef);
+
+    ret[i].battery =
+        (data_packets[i].battery * 3.3 * 4.9) / static_cast<double>(INT12_MAX);
+
+    ret[i].airPressure =
+        (data_packets[i].airPressure * 3.3) / static_cast<double>(INT12_MAX);
+
     ret[i].temperature = data_packets[i].temperature / 333.87 + 21.0;
   }
 
